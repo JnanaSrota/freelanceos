defed54f771e063300ee0bcd9db9fcf66348b636
JnanaSrota <princea747y375@gmail.com>
2026-06-04 17:44:42 +0530

    Add user authentication dependency for JWT token validation

---
+++ app/dependencies.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException,Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.database import get_db
+import os
+from dotenv import load_dotenv
+from jose import jwt,JWTError
+from . import models
+from sqlalchemy.orm import Session
+
+Bearer=HTTPBearer()
+
+SECRET_KEY=os.getenv("JWT_SECRET")
+ALGORITHM=os.getenv("ALGORITHM")
+def get_current_user(credentials:HTTPAuthorizationCredentials=Depends(Bearer), db:Session=Depends(get_db)):
+    token=credentials.credentials
+    try:
+        payload=jwt.decode(token,SECRET_KEY,ALGORITHM)
+        user_id=int(payload.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+    
+    user=db.query(models.User).filter(models.User.id==user_id).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found!")
+    return user
