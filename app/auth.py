7f62e3a405557e7aa34a26bc89ab3b4fe210a82f
JnanaSrota <princea747y375@gmail.com>
2026-06-04 14:26:59 +0530

    Implement user registration and login endpoints with JWT token generation

---
+++ app/auth.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter,HTTPException,Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from datetime import datetime,timedelta
+from jose import jwt
+from app.database import get_db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+router=APIRouter()
+SECRET_KEY=os.getenv("JWT_SECRET")
+ALGORITHM=os.getenv("ALGORITHM")
+pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+
+class RegisterInput(BaseModel):
+    name:str
+    email:str
+    password:str
+    state_code:str
+    initials:str
+    gstin:str=None
+
+class LoginInput(BaseModel):
+    email:str
+    password:str
+
+def create_token(user_id:int):
+    pay_load={
+        "sub":str(user_id),
+        "exp":datetime.utcnow() + timedelta(days=7)
+    }
+    return jwt.encode(payload,SECRET_KEY,ALGORITHM)
+
+
+@router.post("/register")
+def register(data:RegisterInput,db:Session=Depends(get_db)):
+    existing=db.query(models.User).filter(models.User.email==data.email).first()
+    if existing:
+        raise HTTPException(status_code=404,detail="Email already registered")
+
+    user=models.User(
+        name=data.name,
+        email=data.email,
+        password=pwd_context.hash(data.password),
+        state_code=data.state_code,
+        initials=data.initials,
+        gstin=data.gstin
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return {"message":"Registered Successfully"}
+
+
+@router.post("/login")
+def login():
+    user=db.query(models.User).filter(models.User.email==data.email).first()
+    if not user or pwd_context.verify(password,data.password):
+        raise HTTPException(status_code=401, details="Invalid Credentials")
+    return {"token":create_token(user.id)}
--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.database import engine
 from app import models
+from app.database import engine
+
 
 models.Base.metadata.create_all(bind=engine)
-
 app=FastAPI()
 
 @app.get("/")
-def root():
-    return{"message":"RUNNING"}
+def get_root():
+    return {"message":"Running"}
