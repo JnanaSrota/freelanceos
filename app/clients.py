7a84f7e6dec0a58e49b90038e0625895e2da6692
JnanaSrota <princea747y375@gmail.com>
2026-06-04 19:34:08 +0000

    Implement client management endpoints with create, retrieve, and delete functionality

---
+++ app/clients.py
@@ -0,0 +1,47 @@
+from fastapi import HTTPException, Depends, APIRouter
+from pydantic import BaseModel
+from .database import get_db
+from sqlalchemy.orm import Session
+from .dependencies import get_current_user
+
+
+router= APIrouter()
+
+class ClientInput(BaseModel):
+    name:str
+    email:str
+    gstin:Optional[str]=None
+    state_code:Optional[str]=None
+    is_foreign:bool=False
+    tds_applicable:bool=False
+
+@router.post("/")
+def create_client(data:ClientInput,db:Session=Depends(get_db),user=Depends(get_current_user)):
+    client= models.Client( user_id=user.id
+    name=data.name
+    email=data.email
+    gstin=data.gstin
+    state_code=data.state_code
+    tds_applicable=data.tds_applicable
+    is_foreign=data.is_foreign)
+    db.add(client)
+    db.commt()
+    db.refresh(client)
+    return client
+
+@router.get("/")
+def get_client(db:Session=Depends(get_db),user=Depends(get_current_user)):
+    return db.query(models.Client).filter(models.Client.user_id==user.id).all()
+
+@router.delete("/{client_id}")
+def delete_client(client_id:int,db:Session=Depends(get_db),user=Depends(get_current_user)):
+    client=db.query(models.Client).filter(
+        models.Client.id==client_id,
+        models.Client.user_id=user.id
+    ).first()
+    if not client:
+        raise HTTPException(status_code=401,detail="Client Not Found!")
+        db.delete(client)
+        db.commit
+        return {"message":"Client Deleted Successfully"}
+        
--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from app import models
 from app.database import engine
 from app.auth import router as auth_router
-
+from app.clients import router as clients_router
 models.Base.metadata.create_all(bind=engine)
 app=FastAPI()
 app.include_router(auth_router,prefix="/auth")
-
+app.include_router(client_router,prefix="/clients")
 
 @app.get("/")
 def get_root():
