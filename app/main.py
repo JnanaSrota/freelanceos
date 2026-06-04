b3c237148324482ba863562772472634b7b9acc9
JnanaSrota <princea747y375@gmail.com>
2026-06-04 14:28:43 +0530

    Add authentication router to main application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app import models
 from app.database import engine
-
+from app.auth import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
 app=FastAPI()
+app.include_router(auth_router,prefix="/auth")
+
 
 @app.get("/")
 def get_root():
