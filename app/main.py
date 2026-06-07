062621dab205fcbecdf7f031d0c1cc80ad786c3c
JnanaSrota <princea747y375@gmail.com>
2026-06-07 14:33:40 +0530

    Add localhost:3000 to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
         "https://freelanceos-neon.vercel.app",
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "http://192.168.0.101:3000"
+        "http://192.168.0.101:3000",
+        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
