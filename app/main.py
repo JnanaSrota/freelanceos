e1b97506a481cb7d9f4d901a3a5881cf50b9dd8b
JnanaSrota <princea747y375@gmail.com>
2026-06-07 13:47:59 +0530

    Update CORS allowed origins
    Added an additional allowed origin for CORS.

--- app/main.py
+++ app/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
     allow_origins=[
         "https://freelanceos-neon.vercel.app",
         "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "http://192.168.0.101:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
